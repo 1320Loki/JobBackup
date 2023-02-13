749ca70ddbf4447c40988e72f61a915086bb97b0
jose <joseignaciodfg@gmail.com>
2023-02-13 16:22:09 +0000

    mqtt new code

---
+++ MQTT ESP/src/main.cpp
@@ -0,0 +1,20 @@
+/*
+https://patorjk.com/software/taag/#p=display&f=Big&t=MQTT
+  __  __  ____ _______ _______ 
+ |  \/  |/ __ \__   __|__   __|
+ | \  / | |  | | | |     | |   
+ | |\/| | |  | | | |     | |   
+ | |  | | |__| | | |     | |   
+ |_|  |_|\___\_\ |_|     |_|   
+                              
+*/
+
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
