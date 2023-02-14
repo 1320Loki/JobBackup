2fd68592a1f6a278c0cfc22f103bc2e97fa483f3
jose <joseignaciodfg@gmail.com>
2023-02-14 17:28:07 +0000

    Mqtt In

--- MQTT ESP SUB/src/main.cpp
+++ MQTT ESP SUB/src/main.cpp
@@ -35,7 +35,7 @@ const char* password = "esp32wish";
 #define TrialTopic "Trial"
 #define mqtt_server "192.168.1.200"     //  IP of MQTT BROKER
 WiFiClient rtu2;                        //  Name of the MQTT CLIENT
-PubSubClient client(rtu2);
+PubSubClient client(rtu2);              //  CHANGE 
 
 
 
@@ -44,26 +44,28 @@ String change;
 
 //--------------------- Code essencials --------------------//
 
-void conex()
-{
+void conex()  {
+
   Serial.println();
   Serial.println("**************************************");
   Serial.print("Connecting to ");       // begin Wifi connect
   Serial.println(ssid);
+
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   delay(2000);
+
   WiFi.begin(ssid, password);
   int wific = 0;
   
-  while (WiFi.status() != WL_CONNECTED) 
-  {
+  while (WiFi.status() != WL_CONNECTED) {
+
     delay(1000);
     Serial.println("CONECTANDO");
     wific++;
-    if (wific > 5){
-      ESP.restart();
-    }
+
+    if (wific > 5){ ESP.restart();  }
+
   }
   
   Serial.println("");
@@ -72,57 +74,53 @@ void conex()
   Serial.println(WiFi.localIP());
 }
 
-void reconnect() 
-{
-  // Loop until we're reconnected
-  int counter = 0;
-  while (!client.connected()) 
-  {
-    if (counter==2)
-    {
-        ESP.restart();
+void reconnect() {
+
+  while (!client.connected()) {
+
+    Serial.print("Attempting MQTT connection...");
+
+    if (client.connect("")) {
+
+      Serial.println("connected");
+      client.subscribe("Trial");
     }
-    counter+=1;
-    Serial.print("Attempting MQTT connection...\n");
-    // Attempt to connect
-  
-    if (client.connect("rtu2"))
-    {
-        Serial.println("Connected!!!");
-        Serial.println("**************************************\n");
-    } 
-    else 
-    {
-        Serial.print("failed, rc=");
-        Serial.print(client.state());
-        Serial.println(" try again in 5 seconds");
-        // Wait 5 seconds before retrying
-        delay(5000);
+    else  {
+
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
     }
   }
 }
 
-void mqttCallback(char* topic, byte* message, unsigned int length) 
-{
-    String messageTemp;
-    
-    for (int i = 0; i < length; i++) 
-    {
-        //Serial.print((char)message[i]);
-        messageTemp += (char)message[i];
-    }
-    change = messageTemp;
-    Serial.println();
+void callback(char* topic, byte* message, unsigned int length)  {
+
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+
+  Serial.println();
+  if (String(topic) == "Trial") {  Serial.println("Trial arrived"); }
 
 }
 
-void setup() {
+void setup()  {
 
   Serial.begin(115200);
+
   conex();
-  
   client.setServer(mqtt_server, 1883);
-  client.setCallback(mqttCallback);
+  client.setCallback(callback);
 }
 
 void loop() {
@@ -132,9 +130,3 @@ void loop() {
   delay(1000);
 }
 
-/*
-
-
-
-
-*/
