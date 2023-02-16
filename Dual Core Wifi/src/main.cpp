04aaa4791af959956cc6df478c923138e4020805
jose <joseignaciodfg@gmail.com>
2023-02-16 14:42:28 +0000

    16-2-23

--- Dual Core Wifi/src/main.cpp
+++ Dual Core Wifi/src/main.cpp
@@ -1,16 +1,73 @@
 
-/*
+/* https://patorjk.com/software/taag/#p=display&f=Big&t=Dual%20Core
+  _____              _    _____               
+ |  __ \            | |  / ____|              
+ | |  | |_   _  __ _| | | |     ___  _ __ ___ 
+ | |  | | | | |/ _` | | | |    / _ \| '__/ _ \
+ | |__| | |_| | (_| | | | |___| (_) | | |  __/
+ |_____/ \__,_|\__,_|_|  \_____\___/|_|  \___|
+
+*/
+
+//--------------------- essencials Libs --------------------//
 #include <ArduinoJson.hpp>
 #include <ArduinoJson.h>
 #include <PubSubClient.h>
-*/
 #include <Arduino.h>
 #include "WiFi.h"
+//--------------------- essencials Libs --------------------//
 
-#define WIFI_NETWORK "JosePC"
+//--------------------- Code essencials --------------------//
+#define WIFI_NETWORK "JosePC"           //    WIFI
 #define WIFI_PASSWORD "esp32wish"
 #define WIFI_TIME_MS 20000
 
+#define mqtt_server "192.168.1.200"     //  IP of MQTT BROKER
+WiFiClient rtu2;                        //  Name of the MQTT CLIENT
+PubSubClient client(rtu2);
+
+float num;                              //  Trialing and debbuging
+String change;
+//--------------------- Code essencials --------------------//
+
+//////////////////////////////////////////////////////////////
+//-------------------- Created Functions -------------------//
+void reconnect() {
+
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+
+    if (client.connect("")) {
+      Serial.println("connected");
+      client.subscribe("Trial");
+    }
+
+    else  {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    } } }
+
+
+void callback(char* topic, byte* message, unsigned int length)  {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+
+  Serial.println();
+  if (String(topic) == "Trial") {  Serial.println("Trial arrived"); }
+}
+//-------------------- Created Functions -------------------//
+//////////////////////////////////////////////////////////////
+
+//--------------------- TASK 1 >>> WIFI --------------------//
 void WifiAlive (void * parameters) {
 
   for (;;) {
@@ -46,59 +103,53 @@ void WifiAlive (void * parameters) {
     Serial.println("WiFi connected");  
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
+
+  } }
+//--------------------- TASK 1 >>> WIFI --------------------//
+
+//--------------------- TASK 2 >>> MQTT --------------------//
+void task2 (void * parameters)  {
+
+  for (;;)  {
+    reconnect();
   }
 }
+//--------------------- Task 2 >>> MQTT --------------------//
+
+
+
+
+
 
 void setup() {
   // 
   Serial.begin(115200);
-
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  //    task 1
   xTaskCreatePinnedToCore(
-    WifiAlive,
-    "Wifi Alive",
-    5000,
-    NULL,
-    1,
-    NULL,
-    0            //CONFIG_ARDUINO_UDP_RUNNING_CORE
-  );
+    WifiAlive,      //  Function Name
+    "Wifi Alive",   //  Task Name
+    5000,           //  Stack Size
+    NULL,           //  Task Parameters
+    1,              //  Task priority
+    NULL,           //  Task Handle
+    0               //  Core 0 of esp32
+  );            
+
+  //    task 2
+  xTaskCreatePinnedToCore(
+    task2,          //  Function Name
+    "Task 2",       //  Task Name
+    5000,           //  Stack Size
+    NULL,           //  Task Parameters
+    1,              //  Task priority
+    NULL,           //  Task Handle
+    1               //  Core 1 
+  );                
 
 }
 
 void loop() {
-  // 
 }
 
-/*
-
-void conex()  {
-
-}
-
-
-
-
-    if (WiFi.status() == WL_CONNECTED) {
-      Serial.println("Still Connected");
-      vTaskDelay(10000 / portTICK_PERIOD_MS);
-      continue;
-    }
-
-    Serial.println("Connecting!!");
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD);
-
-    unsigned long StartTime = millis();
-  
-    while (WiFi.status()!=WL_CONNECTED && millis()-StartTime< WIFI_TIME_MS) {
-
-      if (WiFi.status() != WL_CONNECTED){
-        Serial.println("WIFI fallo");
-        vTaskDelay(2000 / portTICK_RATE_MS);
-        continue;
-      }
-    }
-
-    Serial.println("[WIFI] Connected: " + WiFi.localIP());
-
-*/
