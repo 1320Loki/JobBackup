32cbad6c94d4b9c767e69bad2da2024f5e0f0ad6
jose <joseignaciodfg@gmail.com>
2023-02-16 09:05:47 +0000

    Dual Core ESP32

--- Dual Core/src/main.cpp
+++ Dual Core/src/main.cpp
@@ -1,9 +1,49 @@
 #include <Arduino.h>
+//  https://www.youtube.com/watch?v=WQGAs9MwXno
+int c1=0, c2=0;
+
+void task1 (void * parameters)  {
+
+  for (;;)  {
+    Serial.print("Task 1 contador: ");
+    Serial.println(c1++);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
+void task2 (void * parameters)  {
+
+  for (;;)  {
+    Serial.print("Task 1 contador: ");
+    Serial.println(c1++);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
 
 void setup() {
-  // put your setup code here, to run once:
+
+  Serial.begin(115200);
+
+  xTaskCreate(
+    task1,
+    "Task 1",
+    1000,
+    NULL,
+    1,
+    NULL
+  );
+
+  xTaskCreate(
+    task2,
+    "Task 2",
+    1000,
+    NULL,
+    1,
+    NULL
+  );
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 }
