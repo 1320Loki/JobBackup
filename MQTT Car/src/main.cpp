153de6641f689e2363f69b68dfdeec0e8757e976
jose <joseignaciodfg@gmail.com>
2023-02-23 11:49:26 +0000

    -

---
+++ MQTT Car/src/main.cpp
@@ -0,0 +1,34 @@
+/*
+https://patorjk.com/software/taag/#p=display&f=Big&t=MQTT
+  __  __  ____ _______ _______ 
+ |  \/  |/ __ \__   __|__   __|
+ | \  / | |  | | | |     | |   
+ | |\/| | |  | | | |     | |   
+ | |  | | |__| | | |     | |   
+ |_|  |_|\___\_\ |_|     |_|   
+                              
+*/
+
+//--------------------- essencials Libs --------------------//
+#include <ArduinoJson.hpp>
+#include <ArduinoJson.h>
+#include <PubSubClient.h>
+#include <Arduino.h>
+#include "WiFi.h"
+//--------------------- essencials Libs --------------------//
+
+//--------------------- Milis function ---------------------//
+unsigned long start;                  // Estructura De millis
+unsigned long current, current2;      //
+const unsigned long period1 = 1500;   //
+const unsigned long period2 = 500;    //
+//--------------------- Milis function ---------------------//
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
