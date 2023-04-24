40e74e745df4e6b51d0047ec5dd83e389e511404
jose <joseignaciodfg@gmail.com>
2023-04-24 15:01:52 +0100

    a

--- Lab Coding/Lab Arduino/Shield 24_4_23/src/main.cpp
+++ Lab Coding/Lab Arduino/Shield 24_4_23/src/main.cpp
@@ -1,9 +1,34 @@
-#include <Arduino.h>
+#include <SPI.h>
+const int CS_PIN = 9; // Pin CS del MCP4231
+
+//////////////////////////////////////////////////////////////////////////////////////////
+void setResistance(int channel, int value) {
+  digitalWrite(CS_PIN, LOW); // Seleccionar el MCP4231
+  SPI.transfer(channel << 7); // Enviar el byte de comando de canal
+  SPI.transfer(value); // Enviar el byte de valor de resistencia
+  digitalWrite(CS_PIN, HIGH); // Deseleccionar el MCP4231
+}
+//////////////////////////////////////////////////////////////////////////////////////////
 
 void setup() {
-  // put your setup code here, to run once:
+
+  pinMode(CS_PIN, OUTPUT); // Establecer el pin CS como salida
+  SPI.begin(); // Iniciar la comunicación SPI
+  delay(100); // Esperar 100ms para asegurarnos de que el MCP4231 esté listo
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  Serial.println("2");
+  setResistance(0, 55);
+  setResistance(1, 55);
+  delay(5000);
+
+  Serial.println("1");
+  setResistance(0, 200);
+  setResistance(1, 200);
+  delay(5000);
 }
+
